99ff10f6c8f1a9cc50458c14cec5c96c949a3f06
JailsonSantos <jailson.ads007@gmail.com>
2022-03-08 10:25:20 -0300

    Desafio Chapter III finalizado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,21 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          {/* O preconnect vai melhorar a performance de carregamento das das fonts na aplicação */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
